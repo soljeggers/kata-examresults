15167bcea407e66b6f9806e687bb87f1a1fad18f
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-09 14:42:28 +0000

    Added Solution for kata-examresults

---
+++ src/main/scala/App/results.scala
@@ -0,0 +1,20 @@
+package App
+
+object results extends App  {
+
+  val correctAns: List[String] = List("A", "B", "C", "D")
+  val userAns: List[String] = List("A", "B", "", "B")
+
+  def checkExamResults(correctAnswers: List[String], answers: List[String]): Int = {
+    val ansCheck: List[(String, String)] = answers.zip(correctAnswers)
+    val result: List[Int] = ansCheck.map(userAnswer => {
+      if (userAnswer._1 == "") 0
+      else if (userAnswer._1 == userAnswer._2) 4
+      else -1})
+    println(ansCheck)
+    println(result)
+    result.sum
+  }
+  println(checkExamResults(correctAns,userAns))
+
+}
